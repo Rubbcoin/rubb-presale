0f9b960b5800b4bdd2d52f97a240737927a6868c
Rubbcoin <agriwealthmain@gmail.com>
2025-12-01 16:43:52 +0500

    Add _app.jsx

---
+++ pages/_app.jsx
@@ -0,0 +1,38 @@
+// pages/_app.jsx
+import { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }) {
+  const network = WalletAdapterNetwork.Mainnet;
+
+  const endpoint =
+    process.env.NEXT_PUBLIC_SOLANA_RPC_URL ||
+    'https://api.mainnet-beta.solana.com';
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
